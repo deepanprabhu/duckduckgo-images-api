93c3401ad06b8a979da81966e3402e70b3bfc3b9
rachmadaniHaryono <foreturiga@gmail.com>
2017-12-19 10:12:32 +0800

    chg: dev: update program
    - fix linter
    - based on python3

--- api.py
+++ api.py
@@ -1,58 +1,63 @@
-import requests;
-import re;
-import json;
-import pprint;
-import time;
+#!/usr/bin/python3
+import requests
+import re
+import json
+import time
+
 
 def search(keywords, max_results=None):
-    url = 'https://duckduckgo.com/';
+    """search images."""
+    url = 'https://duckduckgo.com/'
     params = {
-    	'q': keywords
-    };
+        'q': keywords
+    }
 
     #   First make a request to above URL, and parse out the 'vqd'
     #   This is a special token, which should be used in the subsequent request
     res = requests.post(url, data=params)
-    searchObj = re.search(r'vqd=(\d+)\&', res.text, re.M|re.I);
+    searchObj = re.search(r'vqd=(\d+)\&', res.text, re.M | re.I)
 
     headers = {
-    'dnt': '1',
-    'accept-encoding': 'gzip, deflate, sdch, br',
-    'x-requested-with': 'XMLHttpRequest',
-    'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6,ms;q=0.4',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-    'accept': 'application/json, text/javascript, */*; q=0.01',
-    'referer': 'https://duckduckgo.com/',
-    'authority': 'duckduckgo.com',
+        'dnt': '1',
+        'accept-encoding': 'gzip, deflate, sdch, br',
+        'x-requested-with': 'XMLHttpRequest',
+        'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6,ms;q=0.4',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',  # NOQA
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'referer': 'https://duckduckgo.com/',
+        'authority': 'duckduckgo.com',
     }
 
     params = (
-    ('l', 'wt-wt'),
-    ('o', 'json'),
-    ('q', keywords),
-    ('vqd', searchObj.group(1)),
-    ('f', ',,,'),
-    ('p', '2')
+        ('l', 'wt-wt'),
+        ('o', 'json'),
+        ('q', keywords),
+        ('vqd', searchObj.group(1)),
+        ('f', ',,,'),
+        ('p', '2')
     )
 
-    requestUrl = url + "i.js";
+    requestUrl = url + "i.js"
 
     while True:
-        res = requests.get(requestUrl, headers=headers, params=params);
-        data = json.loads(res.text);
-        printJson(data["results"]);
+        res = requests.get(requestUrl, headers=headers, params=params)
+        data = json.loads(res.text)
+        printJson(data["results"])
         if "next" not in data:
-            exit(0);
-        requestUrl = url + data["next"];
-        time.sleep(5);
+            exit(0)
+        requestUrl = url + data["next"]
+        time.sleep(5)
+
 
 def printJson(objs):
     for obj in objs:
-        print "Width {0}, Height {1}".format(obj["width"], obj["height"]);
-        print "Thumbnail {0}".format(obj["thumbnail"]);
-        print "Url {0}".format(obj["url"]);
-        print "Title {0}".format(obj["title"].encode('utf-8'));
-        print "Image {0}".format(obj["image"]);
-        print "__________";
+        print("Width {0}, Height {1}".format(obj["width"], obj["height"]))
+        print("Thumbnail {0}".format(obj["thumbnail"]))
+        print("Url {0}".format(obj["url"]))
+        print("Title {0}".format(obj["title"].encode('utf-8')))
+        print("Image {0}".format(obj["image"]))
+        print("__________")
 
-search("dora coloring pages");
+
+if __name__ == '__main__':
+    search("dora coloring pages")
