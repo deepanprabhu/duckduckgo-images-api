313159690664752adfaf3b1baa5ba7e56707de45
stuart <stuart@spotlightdata.co.uk>
2018-07-11 15:34:20 +0100

    moved over to python3, make the search function return image urls and made the search function limit the number of urls returned

--- api.py
+++ api.py
@@ -1,58 +1,78 @@
-import requests;
-import re;
-import json;
-import pprint;
-import time;
+import requests
+import re
+import json
+import time
 
-def search(keywords, max_results=None):
+def search(keyword, max_results = 50, refreshes=10):
     url = 'https://duckduckgo.com/';
     params = {
-    	'q': keywords
+    	'q': keyword
     };
-
+    
     #   First make a request to above URL, and parse out the 'vqd'
     #   This is a special token, which should be used in the subsequent request
     res = requests.post(url, data=params)
-    searchObj = re.search(r'vqd=(\d+)\&', res.text, re.M|re.I);
-
+    searchObj = re.search(r'vqd=(\d+)\&', res.text, re.M|re.I)
+    
     headers = {
-    'dnt': '1',
-    'accept-encoding': 'gzip, deflate, sdch, br',
-    'x-requested-with': 'XMLHttpRequest',
-    'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6,ms;q=0.4',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-    'accept': 'application/json, text/javascript, */*; q=0.01',
-    'referer': 'https://duckduckgo.com/',
-    'authority': 'duckduckgo.com',
-    }
-
+        'dnt': '1',
+        'accept-encoding': 'gzip, deflate, sdch, br',
+        'x-requested-with': 'XMLHttpRequest',
+        'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6,ms;q=0.4',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'referer': 'https://duckduckgo.com/',
+        'authority': 'duckduckgo.com',
+        }
+    
     params = (
-    ('l', 'wt-wt'),
-    ('o', 'json'),
-    ('q', keywords),
-    ('vqd', searchObj.group(1)),
-    ('f', ',,,'),
-    ('p', '2')
-    )
-
+        ('l', 'wt-wt'),
+        ('o', 'json'),
+        ('q', keyword),
+        ('vqd', searchObj.group(1)),
+        ('f', ',,,'),
+        ('p', '2')
+        )
+    
     requestUrl = url + "i.js";
 
-    while True:
-        res = requests.get(requestUrl, headers=headers, params=params);
-        data = json.loads(res.text);
-        printJson(data["results"]);
+    image_urls = []
+    
+    for result in range(refreshes):
+        res = requests.get(requestUrl, headers=headers, params=params)
+        
+        try:
+            data = json.loads(res.text)
+        except:
+            print("there was a problem pulling from DuckDuckgo")
+            print(res)
+            print("----")
+            break
+        
+        #scroll through the images on this page and pull out their urls
+        for image in data['results']:
+            
+            im_url = image['image']
+            
+            #check the url has not been found once already
+            if im_url not in image_urls:
+                image_urls.append(im_url)
+                
+            else:
+                print("Double found, %s"%im_url)
+                
+        #if we now have more images than we need we can simply return the
+        #number of urls we wanted
+        if len(image_urls) >= max_results:
+            return image_urls[0:max_results]
+          
+          
+        #if theres no next page we've run out of results and need to stop
         if "next" not in data:
-            exit(0);
-        requestUrl = url + data["next"];
-        time.sleep(5);
-
-def printJson(objs):
-    for obj in objs:
-        print "Width {0}, Height {1}".format(obj["width"], obj["height"]);
-        print "Thumbnail {0}".format(obj["thumbnail"]);
-        print "Url {0}".format(obj["url"]);
-        print "Title {0}".format(obj["title"].encode('utf-8'));
-        print "Image {0}".format(obj["image"]);
-        print "__________";
-
-search("dora coloring pages");
+            break
+        requestUrl = url + data["next"]
+        
+        print("having a sleep to prevent rate limiting")
+        time.sleep(1)
+    
+    return image_urls
