e8cff26da8a3248b5adbd1997c745ddbb9d145a0
Deepan Prabhu Babu <dbabu@hotwire.com>
2018-11-26 15:59:37 -0800

    Imaging api works again !
    1) Fixing token issues
    2) adding to .gitignore

--- api.py
+++ api.py
@@ -1,7 +1,6 @@
 import requests;
 import re;
 import json;
-import pprint;
 import time;
 
 def search(keywords, max_results=None):
@@ -13,17 +12,17 @@ def search(keywords, max_results=None):
     #   First make a request to above URL, and parse out the 'vqd'
     #   This is a special token, which should be used in the subsequent request
     res = requests.post(url, data=params)
-    searchObj = re.search(r'vqd=(\d+)\&', res.text, re.M|re.I);
+    searchObj = re.search(r'vqd=([\d-]+)\&', res.text, re.M|re.I);
 
     headers = {
-    'dnt': '1',
-    'accept-encoding': 'gzip, deflate, sdch, br',
-    'x-requested-with': 'XMLHttpRequest',
-    'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6,ms;q=0.4',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-    'accept': 'application/json, text/javascript, */*; q=0.01',
-    'referer': 'https://duckduckgo.com/',
-    'authority': 'duckduckgo.com',
+        'dnt': '1',
+        'accept-encoding': 'gzip, deflate, sdch, br',
+        'x-requested-with': 'XMLHttpRequest',
+        'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6,ms;q=0.4',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'referer': 'https://duckduckgo.com/',
+        'authority': 'duckduckgo.com',
     }
 
     params = (
@@ -38,13 +37,21 @@ def search(keywords, max_results=None):
     requestUrl = url + "i.js";
 
     while True:
-        res = requests.get(requestUrl, headers=headers, params=params);
-        data = json.loads(res.text);
+        while True:
+            try:
+                res = requests.get(requestUrl, headers=headers, params=params);
+                data = json.loads(res.text);
+                break;
+            except ValueError as e:
+                time.sleep(5);
+                continue;
+
         printJson(data["results"]);
+
         if "next" not in data:
             exit(0);
+
         requestUrl = url + data["next"];
-        time.sleep(5);
 
 def printJson(objs):
     for obj in objs:
@@ -55,4 +62,4 @@ def printJson(objs):
         print "Image {0}".format(obj["image"]);
         print "__________";
 
-search("dora coloring pages");
+search("audi q6");
