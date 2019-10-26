fbfb5dbb1e852feff628148d87593fb94b18c916
Adam Tegtmeier <ategtmei@gmail.com>
2019-10-25 21:56:09 -0400

    Tests are falsly passing.

--- duckduckgo_images_api/test_api.py
+++ duckduckgo_images_api/test_api.py
@@ -1,6 +1,41 @@
 import unittest
+import json
+import os
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 from duckduckgo_images_api import search
+import duckduckgo_images_api
+
+class MockResponse:
+    def __init__(self, value):
+        self.value = value
+
+    @property
+    def text(self):
+        logger.debug("Returning Mock Value")
+        return self.value
+
+def MockPost(*args, **kwargs):
+    logger.debug("MockPost")
+    return MockResponse("other things andvqd=7&more stuff")
+
+with open(os.path.join('duckduckgo_images_api','fixtures.json'), 'r') as handle:
+    FIXTURES = json.load(handle)
+
+def get_fixture():
+    return FIXTURES.pop()
+
+def MockGet(*args, **kwargs):
+    logger.debug("MockGet")
+
+    return MockResponse(get_fixture())
+
+duckduckgo_images_api.api.requests.post = MockPost
+duckduckgo_images_api.api.requests.get = MockGet
 
 class TestApi(unittest.TestCase):
     def setUp(self):
@@ -10,7 +45,15 @@ class TestApi(unittest.TestCase):
         pass
 
     def test_search(self):
-        self.assertEqual(1, 2)
+        self.assertEqual(1, 1)
 
+        results = search("fake search term")
+
+        fullResults = list(results)
+
+        self.assertEqual(len(fullResults), 49)
+
+        #self.assertIn(task_name, task.keys())
+        
 if __name__ == '__main__':
     unittest.main()
